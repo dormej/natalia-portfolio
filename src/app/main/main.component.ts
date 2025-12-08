5c297df9df8c23b04814b41acac26242b95839af
dormej <dorota.mejlun153@gmail.com>
2025-12-08 06:44:00 +0100

    more responsive 1 and 2 view

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+
+  @ViewChild('heroContent') heroContent!: ElementRef<HTMLDivElement>;
+  @ViewChild('heroBg') heroBg!: ElementRef<HTMLDivElement>;
+
+  heroHeight = window.innerHeight;
+
+  ngAfterViewInit() {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+    // fade-out hero-content
+    let opacity = 1 - scrollY / this.heroHeight;
+    opacity = opacity < 0 ? 0 : opacity;
+    this.heroContent.nativeElement.style.opacity = opacity.toString();
+    this.heroBg.nativeElement.style.opacity = opacity.toString();
+  }
+}
